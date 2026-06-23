cbf1cad91b6be7b109cdc815bbd996a860047a97
Carina <cvelazquezr@fctunca.edu.py>
2026-06-23 13:59:28 -0300

    fix: eliminar import SancionesPage inexistente en AppRouter

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -20,7 +20,8 @@ import DetalleLibroUserPage from '../pages/user/DetalleLibroUserPage'
 import CarritoPage from '../pages/user/Carrito'
 import PrestamosPage from '../pages/user/prueba/PrestamosPage'
 import DevolucionesUserPage from '../pages/user/prueba/DevolucionesUserPage'
-import SancionesPage from '../pages/user/prueba/SancionesPage'
+
+//import SancionesPage from '../pages/user/prueba/SancionesPage'
 import PerfilUserPage from '../pages/user/PerfilUserPage'
 
 import AdminDashboardPage from '../pages/admin/DashboardAdmin'
